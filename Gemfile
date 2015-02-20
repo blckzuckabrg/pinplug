43c62f145361971121115a2559417910ee817b37
Aaron Butler <butlera10@gmail.com>
2015-02-20 13:00:05 -0500

    Made change to routes.rb

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'bootstrap-sass', '~> 3.3.3'
 gem 'devise', '~> 3.4.1'
 gem "paperclip", "~> 4.2"
+gem 'aws-sdk', '~> 2.0.26'
 
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   resources :plugs
 
-  devise_for :users
+  devise_for :users 
 
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contact'
-  delete 'users/sign_out' => 'devise/sessions#destroy'
-
+  
+  devise_scope :user do
+    get 'users/sign_out' => "devise/sessions#destroy"
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
